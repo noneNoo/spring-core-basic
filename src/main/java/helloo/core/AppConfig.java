837a39f329f5ae12a6b57530d93cab5063ee5cbd
noneno <noneno@kakao.com>
2021-11-25 21:04:26 +0900

    Refactor: AppConfig 중복 제거

--- src/main/java/helloo/core/AppConfig.java
+++ src/main/java/helloo/core/AppConfig.java
@@ -1,6 +1,8 @@
 package helloo.core;
 
+import helloo.core.discount.DiscountPolicy;
 import helloo.core.discount.FixDiscountPolicy;
+import helloo.core.member.MemberRepository;
 import helloo.core.member.MemberService;
 import helloo.core.member.MemberServiceImpl;
 import helloo.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import helloo.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
